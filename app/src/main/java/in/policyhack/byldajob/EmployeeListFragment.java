fafe819f2d3622fcd71b8a6e869cf103e2e70d17
pujamathur <puja.mathur95@gmail.com>
2015-04-19 04:13:34 +0530

    Send sms and sorting

--- app/src/main/java/in/policyhack/byldajob/EmployeeListFragment.java
+++ app/src/main/java/in/policyhack/byldajob/EmployeeListFragment.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.zip.Inflater;
@@ -20,6 +23,7 @@ public class EmployeeListFragment extends android.support.v4.app.Fragment {
 
     private ListView listView;
     private Adapter adapter;
+    private Button sendSms;
 
 
     public static EmployeeListFragment newInstance() {
@@ -42,9 +46,25 @@ public class EmployeeListFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_employee_list, container, false);
+        for(int i = 0; i < 3; i++){
+            Employee employee = new Employee();
+            employee.name = "Name";
+            employee.state = "State";
+            employee.district = "District";
+            employee.adhaarNumber = 12.5;
+            employee.mobileNumber = 10.0;
+            Employee.chosenEmployee.add(employee);
+        }
         listView = (ListView) v.findViewById(R.id.listView);
+        sendSms = (Button) v.findViewById(R.id.smsButton);
         adapter = new Adapter(getActivity().getApplicationContext());
         listView.setAdapter(adapter);
+        sendSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage("");
+            }
+        });
         return v;
     }
 
@@ -102,6 +122,22 @@ public class EmployeeListFragment extends android.support.v4.app.Fragment {
             district.setText(Employee.chosenEmployee.get(position).district);
             aadhar.setText(Employee.chosenEmployee.get(position).adhaarNumber + "");
             return v;
+        }
+    }
+
+    public void sendSMSMessage(String phoneNo) {
+        String message = "Hey";
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getActivity().getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "SMS faild, please try again.",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
         }
     }
 
