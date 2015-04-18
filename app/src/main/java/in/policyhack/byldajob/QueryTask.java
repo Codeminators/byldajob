a9a1ab90ba27af7dd3eb5706837904e498d41d62
Prempal Singh <prempal.42@gmail.com>
2015-04-18 20:38:48 +0530

    query task

---
+++ app/src/main/java/in/policyhack/byldajob/QueryTask.java
@@ -0,0 +1,51 @@
+package in.policyhack.byldajob;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by prempal on 18/4/15.
+ */
+public class QueryTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        String response = null;
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("the url goes here");
+        try {
+
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("query", strings[0]));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            response = httpclient.execute(httppost, responseHandler);
+            Log.d("Response", response);
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+    }
+}
