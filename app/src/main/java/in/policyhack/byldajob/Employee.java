efc8583136bfaed18a8b4a29f68bb59dc7c6bbdf
pujamathur <puja.mathur95@gmail.com>
2015-04-19 01:54:30 +0530

    rating basic algorithm

--- app/src/main/java/in/policyhack/byldajob/Employee.java
+++ app/src/main/java/in/policyhack/byldajob/Employee.java
@@ -20,6 +20,21 @@ public class Employee {
     String speciality;
     String certificateAvailable;
     int marks;
-    String retention;
+    int retention;
+    int lastEmployedAt;
+
+    public int rateEmployee(Employee employee){
+        int rating = 0;
+        int workexrating = 2*employee.workEx/4;
+        int marksRating = 1*employee.marks/100;
+        int lastEmployedRating = 2*employee.lastEmployedAt/6;
+        int retentionRating = 2*employee.retention/6;
+        int policeVerificationRating = 2;
+        if(employee.policeVerification.equals("NO")){
+            policeVerificationRating = 0;
+        }
+        rating = (workexrating+marksRating+lastEmployedRating+retentionRating+policeVerificationRating)/9;
+        return rating;
+    }
 
 }
